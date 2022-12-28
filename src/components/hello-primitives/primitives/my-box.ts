1dfb7b512d6084e8df0d4629a624c2ce36f85b73
Yungsun <yungsun3307@163.com>
2022-12-28 13:40:22 +0800

    feat: 添加 CylinderGeometry

--- src/components/hello-primitives/primitives/my-box.ts
+++ src/components/hello-primitives/primitives/my-box.ts
@@ -1,7 +1,7 @@
 import {BoxGeometry} from "three";
 
 const width = 8
-const height = 8
-const depth = 8
+const height = 5
+const depth = 5
 
 export const myBox = new BoxGeometry(width, height, depth);
---
+++ src/components/hello-primitives/primitives/my-cylinder.ts
@@ -0,0 +1,7 @@
+import {CylinderGeometry} from "three";
+
+const radiusTop = 3
+const radiusBottom = 5
+const height = 5
+
+export const myCylinder = new CylinderGeometry(radiusTop, radiusBottom, height)
